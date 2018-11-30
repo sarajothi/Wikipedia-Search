d31443b3ccf9bdadf42cfa93891326f044a30557
abhilash <abhilash@ordant.com>
2018-11-30 12:03:45 +0530

    Indentation

--- src/components/WikiList.js
+++ src/components/WikiList.js
@@ -5,19 +5,19 @@ const WikiList = (props) => {
   const { loading, items } = props;
 
   return !loading ? (
-      <div className="wiki-list">
-          {
-              Object.keys(items).map(key => (
-                  <div key={key} className="panel panel-default">
-                      <div className="panel-heading" >
-                          <a rel="noopener noreferrer" href={`https://en.wikipedia.org/?curid=${key}`} target="_blank"> {items[key].title}
-                          </a>
-                      </div>
-                      <div className="panel-body">{items[key].extract}</div>
-                  </div>
-              ))
-          }
-      </div>
+    <div className="wiki-list">
+      {
+        Object.keys(items).map(key => (
+          <div key={key} className="panel panel-default">
+            <div className="panel-heading" >
+              <a rel="noopener noreferrer" href={`https://en.wikipedia.org/?curid=${key}`}        target="_blank"> {items[key].title}
+              </a>
+            </div>
+            <div className="panel-body">{items[key].extract}</div>
+          </div>
+        ))
+      }
+    </div>
   ) : <Loader />
 };
 
